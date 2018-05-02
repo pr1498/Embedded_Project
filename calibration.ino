2daeaec3acbf166e2c65ca6b9b085aade098dd51
EnboLiu <593200759@qq.com>
2018-05-02 15:13:01 -0400

    Update calibration.ino

--- calibration.ino
+++ calibration.ino
@@ -1,5 +1,5 @@
 int value;
-void calibration() {
+int calibration() {
   value = adc->analogRead(POT_PIN); // read a new value, will return ADC_ERROR_VALUE if the comparison is false.
   Serial.print("Pin: ");
   Serial.print(POT_PIN);
