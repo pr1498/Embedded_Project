0ee9e4aa4f0040f8b9209bd678f133e0259e6adb
Jasonwang23 <wangshenjason@gmail.com>
2018-05-04 17:38:48 -0400

    Add files via upload

--- calibration.ino
+++ calibration.ino
@@ -1,11 +1,10 @@
 int value;
 void calibration() {
-//  value = adc->analogRead(POT_PIN); // read a new value, will return ADC_ERROR_VALUE if the comparison is false.
-//  Serial.print("Pin: ");
-//  Serial.print(POT_PIN);
-//  Serial.print(", value ADC1: ");
-//  Serial.println(value * 3.3 / adc->getMaxValue(ADC_1), DEC);
-  
-
+  value = adc->analogRead(POT_PIN); // read a new value, will return ADC_ERROR_VALUE if the comparison is false.
+  Serial.print("Pin: ");
+  Serial.print(POT_PIN);
+  Serial.print(", value: ");
+  value = map(value, 0, 1023, 0, 90);
+  Serial.println(value,DEC);
 }
 
---
+++ supercalib.ino
@@ -0,0 +1,7 @@
+void supercalib ()
+{
+calibration();
+forward(20,25);
+delay(5000);
+}
+
