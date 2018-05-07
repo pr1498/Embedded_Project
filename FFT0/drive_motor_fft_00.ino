8ec97d09a9b4d665c88f27b983e105f876c7f570
pr1498 <31830465+pr1498@users.noreply.github.com>
2018-05-07 11:47:29 -0400

    Add files via upload

--- FFT0/drive_motor_fft_00.ino
+++ FFT0/drive_motor_fft_00.ino
@@ -3,6 +3,7 @@
 
 arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
 
+//------PIN numbers-------------//
 #define SWITCH_PIN_3 5
 #define SWITCH_PIN_1 6
 #define SWITCH_PIN_2 7
@@ -13,6 +14,12 @@ arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
 #define MIC_PIN A0
 #define PROX_TRIG_PIN 23
 #define PROX_ECHO_PIN 22
+#define PROX_TRIG_PIN_right 21
+#define PROX_ECHO_PIN_right 20
+#define PROX_TRIG_PIN_left  3
+#define PROX_ECHO_PIN_left 4
+//--------------------------------//
+
 #define AVG_NUM   0xA //number of ffts to be averaged
 #define INVALID_VAL   250 //outside the valid count of frequencies, we are only going to get 10 frequencies at max
 #define NUM_TURNS     15
@@ -20,7 +27,7 @@ arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
 //------Frequencies to detect-----------//
 #define LAST_FREQUENCY 9500
 #define SAMPLING_FREQUENCY 20000
-#define NUM_FREQ   10 
+#define NUM_FREQ   10
 //--------------------------------------//
 
 
@@ -38,12 +45,13 @@ void setup() {
   Serial.begin(115200);
   pinMode(PROX_TRIG_PIN, OUTPUT); // Sets the trigPin as an Output
   pinMode(PROX_ECHO_PIN, INPUT); // Sets the echoPin as an Input
+  pinMode(PROX_TRIG_PIN_left, OUTPUT);
+  pinMode(PROX_ECHO_PIN_left, INPUT);
+  pinMode(PROX_TRIG_PIN_right, OUTPUT);
+  pinMode(PROX_ECHO_PIN_right, INPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(MIC_PIN, INPUT);
-  pinMode(SWITCH_PIN_1, INPUT);
-  pinMode(SWITCH_PIN_2, INPUT);
-  pinMode(SWITCH_PIN_3, INPUT);
-  pinMode(POT_PIN, INPUT);
+
 
   sampling_period_us = round(1000000 * (1.0 / samplingFrequency));
   for (int i = 0; i < NUM_FREQ; i++) {
@@ -69,7 +77,7 @@ uint32_t freq_amp_new[NUM_FREQ];
 uint32_t max_freq_amp[NUM_FREQ];
 bool tone_done[NUM_FREQ]; //1 for each tone
 uint8_t freq_to_detect = 0;
-uint16_t dist;
+uint16_t dist, dist_left, dist_right;
 bool new_frequency = 0;
 bool finish = 0;
 bool redo = 0;
@@ -97,13 +105,34 @@ void loop() {
       turn(1, max_degree[freq_to_detect]);
       direction = 1;
     }
-    //if(redo != 1) prev_max_freq_amp = max_freq_amp[];
+
+//    //since we only sample for half a second , we might turn an extra amount, depending upon where we sampled the max.
+//    //but this doesn't matter if it's not the last beacon
+//    if (frequency[freq_to_detect] == LAST_FREQUENCY) {
+//      float freq_amp1 = 0, freq_amp2 = 0, freq_amp3 = 0;
+//      for (int i = 0; i < 2; i++) {
+//        sample_data();
+//        freq_amp1 += total_avg[freq_to_detect];
+//      }
+//      turn(0, (360 / NUM_TURNS));
+//      for (int i = 0; i < 2; i++) {
+//        sample_data();
+//        freq_amp2 += total_avg[freq_to_detect];
+//      }
+//      turn(1, (2 * 360 / NUM_TURNS));
+//      for (int i = 0; i < 2; i++) {
+//        sample_data();
+//        freq_amp3 += total_avg[freq_to_detect];
+//      }
+//      if(freq_amp2 > freq_amp1 && freq_amp2 > freq_amp3) turn(0, (2 * 360/NUM_TURNS));
+//      else if(freq_amp1 > freq_amp2 && freq_amp1 > freq_amp3) turn(0, (360/NUM_TURNS));
+//    }
 
     //if finish is 1 or redo is 1 or tone_done is 1 break
     while (finish != 1 && redo != 1 && tone_done[freq_to_detect] != 1 ) {
 
       move_forward();
-      
+
       //reset to 0
       for (int freq = 0; freq < NUM_FREQ; freq++) {
         freq_amp_new[freq] = 0;
@@ -130,6 +159,14 @@ void loop() {
           finish = 1;
           Serial.println("This is the last frequency. Mission completed !!");
           digitalWrite(LED_PIN, 1);
+        }
+      }
+      //since we do a 360 turn if redo is set, want to check left and right distance
+      else if (redo == 1) {
+        //if close to left or right, move forward slightly
+        //hopefully there won't be any beacon right in front when we try to move
+        if (dist_right < 15 || dist_left < 15) {
+          forward(20, 10);
         }
       }
     }
@@ -160,8 +197,10 @@ void loop() {
 }
 
 void move_forward() {
-  dist = detect_distance();
+  dist      = detect_distance();
+
   if (dist > 400) dist = 0;
+
   Serial.print("Distance ");
   Serial.println(dist);
   if (dist > 30) {
@@ -180,8 +219,12 @@ void move_forward() {
     }
   }
 
-  //update distance again, since this is used by the main loop to 
+  //update distance again, since this is used by the main loop to
   //determine if we have reached the desired beacon or not
-  dist = detect_distance();
-  if(dist > 400) dist = 0;
+  dist       = detect_distance();
+  dist_left  = detect_distance_left();
+  dist_right = detect_distance_right();
+  if (dist > 400) dist = 0;
+  if (dist_left > 400) dist_left = 0;
+  if (dist_right > 400) dist_right = 0;
 }
